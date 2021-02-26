3f14afdbe9aaf4318bc365425f24c9e370013cc2
iyume <iyumelive@gmail.com>
2021-02-26 22:40:21 +0800

    [Backend] fix pylance error and db close error

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,11 +1,11 @@
-from typing import Generator
+from typing import Any, Generator
 
 from app.db.load_db import SessionMaker
 
 def get_db() -> Generator:
+    db: Any = None # escape pylance
     try:
         db = SessionMaker()
         yield db
     finally:
-        ...
-        # db.close()
+        db.close()
--- backend/app/db/load_db.py
+++ backend/app/db/load_db.py
@@ -3,5 +3,11 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import config
 
-engine = create_engine(config.DATABASE_URI)
+engine = create_engine(
+    config.DATABASE_URI,
+    connect_args={
+        'check_same_thread': False
+    }
+)
+
 SessionMaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
