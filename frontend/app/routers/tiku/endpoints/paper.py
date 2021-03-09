caa4cfb3561e3d56a09741abda3d8e012fb7bc62
iyume <iyumelive@gmail.com>
2021-03-09 10:34:35 +0800

    🔥 [Frontend] remove unused code

--- frontend/app/routers/tiku/endpoints/paper.py
+++ frontend/app/routers/tiku/endpoints/paper.py
@@ -20,36 +20,15 @@ router = APIRouter(prefix='/paper')
 async def tiku_paper(
     request: Request,
     subject: str,
-    type: str = None,
-    id: Optional[str] = None,
-    picked: Optional[str] = None,
+    type: str,
     subjects: Subjects = Depends(deps.get_subjects),
     current_user: UserPayload = Depends(deps.get_current_user)
 ) -> Any:
     """
-    Case1:
-        Render random question-select practice page
-    Case2:
-        Render answer page according to selected option
-    if (id & picked) -> Case2
+    Render random question-select practice page
     """
-    ## Case 2
-    if id and picked:
-        question = await apifunc.get_answer(id)
-        return templates.TemplateResponse(
-            'tiku/paper/subject.jinja2', {
-                'request': request,
-                'current_user': current_user,
-                'question': question,
-                'subjects': subjects
-            }
-        )
-
-    ## Case 1
     if subject not in subjects.aliases:
         raise HTTPException(status_code=404, detail='Subject not found')
-    if not type:
-        raise HTTPException(status_code=422, detail='Missing type parameter')
     if type == 'random':
         question = await apifunc.get_question_by_subject(subject)
         return templates.TemplateResponse(
@@ -62,19 +41,3 @@ async def tiku_paper(
         )
     else:
         raise HTTPException(status_code=400, detail='Bad request')
-
-
-@router.post('/{subject}', response_class=HTMLResponse)
-async def tiku_paper_action(
-    request: Request,
-    subject: str,
-    subjects: Subjects = Depends(deps.get_subjects),
-    current_user: UserPayload = Depends(deps.get_current_user)
-) -> Any:
-    """
-    Render answer page, `subject` for a return link
-    """
-    form = await request.form()
-    if 'id' not in form and 'picked' not in form:
-        raise HTTPException(status_code=400, detail='Bad request')
-    return form
