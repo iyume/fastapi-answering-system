d099fa0f447ad73c5ede88e0ec3ab7338c685fab
iyume <iyumelive@gmail.com>
2021-03-10 01:03:22 +0800

    ✅ add simple exam_page

--- frontend/app/routers/exam/exam.py
+++ frontend/app/routers/exam/exam.py
@@ -1,4 +1,5 @@
 from typing import Any
+from datetime import datetime
 
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
@@ -20,9 +21,27 @@ async def exam_entry(
     current_user: schema.UserPayload = Depends(deps.get_current_user)
 ) -> Any:
     exams = await apifunc.exam_fetchall()
+    unstarted_exams: list[dict] = []
+    processing_exams: list[dict] = []
+    finished_exams: list[dict] = []
+    for exam in exams:
+        exam['start_time'] = datetime.fromisoformat(exam['start_time'])
+        exam['end_time'] = datetime.fromisoformat(exam['end_time'])
+        if exam['start_time'] > datetime.now():
+            unstarted_exams.append(exam)
+        if exam['end_time'] < datetime.now():
+            finished_exams.append(exam)
+        if exam['start_time'] < datetime.now() < exam['end_time']:
+            processing_exams.append(exam)
+    for exam in exams:
+        exam['start_time'] = datetime.strftime(exam['start_time'], '%F %X')
+        exam['end_time'] = datetime.strftime(exam['end_time'], '%F %X')
     return templates.TemplateResponse(
         'exam/entry.jinja2', {
             'request': request,
-            'current_user': current_user
+            'current_user': current_user,
+            'unstarted_exams': unstarted_exams,
+            'processing_exams': processing_exams,
+            'finished_exams': finished_exams
         }
     )
