9d0157b4011501989e9f5cb36e1fa4c70bcbdf9d
iyume <iyumelive@gmail.com>
2021-03-24 11:45:30 +0800

    🚸 🎨 Improve params and template variable deliver

--- frontend/app/routers/tiku/paper.py
+++ frontend/app/routers/tiku/paper.py
@@ -15,12 +15,11 @@ from app.security import login_required
 router = APIRouter(prefix='/paper')
 
 
-@router.get('/{subject}', response_class=HTMLResponse)
+@router.get('/{subject}/random', response_class=HTMLResponse)
 @login_required
-async def tiku_paper(
+async def tiku_paper_random(
     request: Request,
     subject: str,
-    qtype: str,
     subjects: Subjects = Depends(deps.get_subjects),
     current_user: UserPayload = Depends(deps.get_current_user)
 ) -> Any:
@@ -29,17 +28,26 @@ async def tiku_paper(
     """
     if subject not in subjects.aliases:
         raise HTTPException(status_code=404, detail='Subject not found')
-    if qtype == 'random':
-        question = await apifunc.get_question_by_subject(subject)
-        return templates.TemplateResponse(
-            'paper/practice_random.jinja2', {
-                'request': request,
-                'current_user': current_user,
-                'question': question,
-                'subjects': subjects
-            }
-        )
-    if qtype == 'order':
-        raise HTTPException(status_code=200, detail='进行中的工作')
-    else:
-        raise HTTPException(status_code=400, detail='Bad request')
+    question = await apifunc.get_question_by_subject(subject)
+    return templates.TemplateResponse(
+        'paper/practice_random.jinja2', {
+            'request': request,
+            'current_user': current_user,
+            'question': question,
+            'subjects': subjects
+        }
+    )
+
+
+@router.get('/{subject}/order/{order}', response_class=HTMLResponse)
+@login_required
+async def tiku_paper_order(
+    request: Request,
+    subject: str,
+    order: int,
+    subjects: Subjects = Depends(deps.get_subjects),
+    current_user: UserPayload = Depends(deps.get_current_user)
+) -> Any:
+    """
+    order practice page
+    """
